54c53db3ff360aebf910c19053ed9861312bfc0b
David Su <dtscraft@gmail.com>
2013-06-20 00:35:16 -0700

    used python-dateutil library to parse facebook event datetime
    facebook events have 3 different formats for time, all of which is
    handled by python-dateutil
    python-dateutil has also been added to requirements.txt

--- fb_events.py
+++ fb_events.py
@@ -1,6 +1,22 @@
 import os,re, urllib2
+from dateutil.parser import parse
+from datetime import datetime
+import facebook
+import pdb
 
+"""
+simple library for retrieving events from facebook groups
+include the following in local_settings.py in your project directory
+https://developers.facebook.com/tools/explorer/
+APP_ID = ''
+APP_SECRET = ''
 USER_ACCESS_TOKEN = ''
+"""
+APP_ID = None
+APP_SECRET = None
+USER_ACCESS_TOKEN = None
+
+
 
 if os.path.exists(os.path.normpath(os.path.join(os.path.dirname(__file__), 'local_settings.py'))):
   from local_settings import *
@@ -22,14 +38,22 @@ class FacebookGroup(object):
     Both facebook group and pages can have events.
     The object is meant only for publicly available groups and pages.
     Read more at https://developers.facebook.com/
+    TODO: handle invalid access token better
     Retrieve your access token here https://developers.facebook.com/tools/explorer/
     Inspiration from fbconsole and facebook-sdk
     '''
     def __init__(self, url, access_token = USER_ACCESS_TOKEN):
+        import urllib2
+        try:
+            self.user_access_token = access_token
+            print self.user_access_token
+            self.access_token = self.user_access_token if self.user_access_token != None else facebook.get_app_access_token(APP_ID, APP_SECRET)
+        except urllib2.HTTPError, e:
+            response = _parse_json(e.read())
+            raise facebook.GraphAPIError(response)
         self.url = url.strip()
         self.id = None
         self.events = []
-        self.access_token = access_token
     def get_id(self):
         '''
         Method to retrieve names from facebook pages and groups.
@@ -86,17 +110,34 @@ class FacebookGroup(object):
     def get_events(self):
         """
         Get a set of events for facebook page and facebook groups
-        TODO: complete parser
+        There are 3 cases: date-only, precise time and local time(deprecated)
+        TODO: handle invalid access token esp. for public vs. private groups like  hackers at berkeley
+        http://developers.facebook.com/docs/reference/api/event/
         """
-        data = urllib2.urlopen('http://graph.facebook.com/'+self.id+'/events?access_token='+self.access_token).read()
-        response = _parse_json(data)
-        pass
+        try:
+            data = urllib2.urlopen('https://graph.facebook.com/'+str(self.id)+'/events?access_token='+self.access_token).read()
+            response = _parse_json(data)
+            self.events = response['data']
+            for event in self.events:
+                event['start_time'] = parse(event['start_time'])
+                if 'end_time' in event:
+                    event['end_time'] = parse(event['end_time'])
+                if 'updated_time' in event:
+                    event['updated_time'] = parse(event['updated_time'])
+            return self.events
+        except urllib2.HTTPError, e:
+            response = _parse_json(e.read())
+            raise facebook.GraphAPIError(response)
+
     def generate_event_url(event_id):
         return 'https://www.facebook.com/events/'+event_id
 
 def main():
     import os
-    os.system('python -m doctest %s' % __file__)
+    #os.system('python -m doctest %s' % __file__)
+    hb = FacebookGroup('https://graph.facebook.com/276905079008757')
+    #http://stackoverflow.com/questions/1302161/how-do-i-parse-timezones-with-utc-offsets-in-python
+
 
 if __name__ == "__main__":
     main()
