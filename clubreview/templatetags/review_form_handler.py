ab3a714662d5787d2311a6961242a767242083e9
Daniel Liu <hpec@qq.com>
2013-06-27 00:40:15 -0700

    add success msg and fix bug

--- clubreview/templatetags/review_form_handler.py
+++ clubreview/templatetags/review_form_handler.py
@@ -9,29 +9,22 @@ from clubreview.forms import *
 
 register = template.Library()
 
-
 @register.inclusion_tag('review_form.html', takes_context=True)
 def review_form_handler(context):
-    #print context
     initials = {}
     if 'club' in context:
         initials['club'] = context['club']
+
     request = context['request']
     if request.method == 'POST':
         form = ReviewForm(data=request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Review submitted successfully')
-            print "Validation Successful!"
-        else:
-            messages.error(request, 'Form Error')
-            print "Form Error"
-            print form.errors
-        return {'form':form} #HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+            form = ReviewForm(initial=initials)
+            form.success = "Review submitted successfully"
     else:
         form = ReviewForm(initial=initials)
     return {'form':form}
-
 
 #t = get_template('review_form.html')
 #register.inclusion_tag(t)(review_form_handler, takes_context=True)
