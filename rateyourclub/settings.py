b285f99f9be61de77f9eb3d3473c8e7120fdd440
Daniel Liu <hpec@qq.com>
2013-07-22 01:03:13 -0700

    refactor templates paths

--- rateyourclub/settings.py
+++ rateyourclub/settings.py
@@ -112,7 +112,7 @@ ROOT_URLCONF = 'rateyourclub.urls'
 WSGI_APPLICATION = 'rateyourclub.wsgi.application'
 
 TEMPLATE_DIRS = (
-    ROOTDIR+'/templates/',
+    # ROOTDIR+'/templates/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
