fe734ff480d61474385d1b362591112991cf7aba
David Su <dtscraft@gmail.com>
2013-08-11 16:32:47 -0700

    add url contributions via AJAX
    also displays corresponding success/error messages
    fixes #13
    
    Conflicts:
    
    	clubreview/views.py

--- clubreview/views.py
+++ clubreview/views.py
@@ -3,9 +3,13 @@ from models import *
 from forms import *
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core import serializers
+from django.core.exceptions import ValidationError
+import itertools
+from django.utils import simplejson
+
 from django.contrib import messages
 
 def club_list_view(request, template_name='club_list.html'):
@@ -62,16 +66,54 @@ def create_review(request):
 def add_url_edit(request, id):
     if request.method == 'POST':
         club = get_object_or_404(Club, pk=id)
+        c1, c2 = None, None
+        to_json = {}
+        errors = []
+        success_messages = []
         if request.POST['facebook_url']:
-            c = ClubURIEdit(club=club)
-            c.value = request.POST['facebook_url']
-            c.attribute_type = ClubURIEdit.FACEBOOK_URL_TYPE
-            c.save()
+            c1 = ClubURIEdit(club=club)
+            c1.value = request.POST['facebook_url']
+            c1.attribute_type = ClubURIEdit.FACEBOOK_URL_TYPE
+            try:
+                c1_errors = None
+                c1.full_clean()
+            except ValidationError as e:
+                c1_errors = []
+                value_error = e.message_dict.pop('value', None)
+                if value_error:
+                    c1_errors.append(["Please enter a valid facebook url"])
+                c1_errors.append(itertools.chain(e.message_dict.values()))
+                errors.append(itertools.chain(*c1_errors))
+            if not c1_errors:
+                success_messages.append('Added facebook url: %s' % c1.value)
         if request.POST['website_url']:
-            c = ClubURIEdit(club=club)
-            c.value = request.POST['website_url']
-            c.attribute_type = ClubURIEdit.WEBSITE_URL_TYPE
-            c.save()
-        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+            c2 = ClubURIEdit(club=club)
+            c2.value = request.POST['website_url']
+            c2.attribute_type = ClubURIEdit.WEBSITE_URL_TYPE
+            try:
+                c2_errors = None
+                c2.full_clean()
+            except ValidationError as e:
+                c2_errors = []
+                value_error = e.message_dict.pop('value', None)
+                if value_error:
+                    c2_errors.append(["Please enter a valid website url"])
+                c2_errors.append(itertools.chain(e.message_dict.values()))
+                errors.append(itertools.chain(*c2_errors))
+            if not c2_errors:
+                success_messages.append('Added website url: %s' % c2.value)
+        if len(errors) > 0:
+            to_json['message'] = ', '.join(itertools.chain(*errors))
+            return HttpResponse(content=simplejson.dumps(to_json), mimetype='application/json', status=422)
+        elif any([c1,c2]):
+            if c1:
+                c1.save()
+            if c2:
+                c2.save()
+            to_json['message'] = ', '.join(success_messages)
+            return HttpResponse(content=simplejson.dumps(to_json), mimetype='application/json')
+        else:
+            to_json['message'] = 'Facebook and/or website urls expected'
+            return HttpResponse(content=simplejson.dumps(to_json), mimetype='application/json', status=422)
     else :
         raise Http404
