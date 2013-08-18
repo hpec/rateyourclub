1a154ea05ac781f56344a269c0f6a52b2e8cea1e
Daniel Liu <hpec@qq.com>
2013-08-18 13:41:46 -0700

    support tokenized search

--- clubreview/lookups.py
+++ clubreview/lookups.py
@@ -14,6 +14,7 @@ registry.register(ClubLookUp)
 
 
 def club_lookup(request):
+    #TODO: refactor and share this search logic with club_list_view
     result = []
     query = request.GET.get('term')
     querySet = Club.objects.all()
